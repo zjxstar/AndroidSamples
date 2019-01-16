ca644bef347d918e3fc0c05d7de7ef9ec42d667b
zhoujingxin <zhoujingxin@360.cn>
2019-01-16 10:06:46 +0800

    添加Md5部分

--- SecuritySample/app/src/main/java/com/zjx/securitysample/MainActivity.kt
+++ SecuritySample/app/src/main/java/com/zjx/securitysample/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.zjx.securitysample
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun toMD5Page(view: View) {
+        val intent = Intent(this, MD5Activity::class.java)
+        startActivity(intent)
     }
 }
